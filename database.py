3168e9a06fdba64779d2e05f471904008c2638be
Sawnd <sawnd.the.one@gmail.com>
2018-06-04 10:47:25 +0200

    cleaning

--- database.py
+++ database.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python
-import mysql.connector as connector
 
+#représente la connexion à la base de données
+
+import pymysql.cursors
 # Connect
-db = connector.connect(host="localhost",
+db = pymysql.connect(host="localhost",
                        user="root",
                        passwd="",
                        db="eb04")
 
 cursor = db.cursor()
 
-# Execute SQL select statement
-#cursor.execute("SELECT * FROM temperature")
-# Commit your changes if writing
-# In this case, we are only reading data
-# db.commit()
-
-# Get the number of rows in the resultset
-# numrows = cursor.rowcount
-#
-# # Get and display one row at a time
-# for x in range(0, numrows):
-#     row = cursor.fetchone()
-#     print row[0], "-->", row[1]
-#
-# # Close the connection
-# db.close()
--- test.py
+++ test.py
@@ -1,33 +1,12 @@
 #from HTU21D import HTU21D
-#import database
-import pymysql.cursors
 import datetime
+import database
 
 
-db = pymysql.connect(host="localhost",
-                       user="root",
-                       passwd="",
-                       db="eb04")
-
-cursor = db.cursor()
-timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#On récupère la  température et on l'enregistre dans al base de données
+#timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 #temperature = HTU21D(1).read_temperature()
-temperature = 22
-query = "INSERT INTO Temperature ('Valeur') VALUES ('?')"
-query2 ="SELECT * FROM Temperature"
-
-cursor.execute("INSERT INTO Temperature (Valeur) VALUES ('%d') " % temperature)
-#cursor.execute(query2)
-db.commit()
-results = cursor.fetchall()
-for a in results:
-    if a:
-        print('True')
-    else:
-        print('False')
-
-
-#rows = cursor.fetchall()
-#print (rows)
-print(results)
+temperature = 35
+database.cursor.execute("INSERT INTO Temperature (Valeur) VALUES ('%d') " % temperature)
+database.db.commit()
 
