1a8b284020cd038070a41c2140ba87286bce4e3a
Sawnd <sawnd.the.one@gmail.com>
2018-06-04 11:02:14 +0200

    mise en place d'u scheduler

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 #from HTU21D import HTU21D
 import datetime
 import database
+import schedule
+import time
 
 
-#On récupère la  température et on l'enregistre dans al base de données
+def job():
+    temperature = 35
+    database.cursor.execute("INSERT INTO Temperature (Valeur) VALUES ('%d') " % temperature)
+    database.db.commit()
+    print(temperature)
+
+
+schedule.every(10).seconds.do(job)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+#On récupère la  température et on l'enregistre dans la base de données
 #timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 #temperature = HTU21D(1).read_temperature()
-temperature = 35
-database.cursor.execute("INSERT INTO Temperature (Valeur) VALUES ('%d') " % temperature)
-database.db.commit()
+
+
 
