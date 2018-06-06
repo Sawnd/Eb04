9447b83cf6b4db8129d499788ea6eeb773e7b79c
miqreh <thibaut.counot@gmail.com>
2018-06-06 14:22:55 +0100

    Ajout dans les db

--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Steph\PycharmProjects\Eb04\venv\Scripts\python.exe
+#!C:\Users\thibaut\PycharmProjects\Eb04\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==9.0.3','console_scripts','pip3'
 __requires__ = 'pip==9.0.3'
 import re
