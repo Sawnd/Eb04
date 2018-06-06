59b8abe4b7c4f47dfa44f73c315c7cf6b3fc4f5f
miqreh <thibaut.counot@gmail.com>
2018-06-06 14:06:34 +0100

    Mesure de l'ensoleillemeny via la camera

---
+++ measure_brightness.py
@@ -0,0 +1,30 @@
+
+from scipy import misc
+from picamera import PiCamera
+from time import sleep
+
+def b_measure(image):
+    "Mesure de la luminosité relaitve"
+    fichier=image+'.jpg'
+    img = misc.imread(fichier)
+    Y = []
+    for i in range(0,len(img)-1):
+            Y=[0.2126*img[i][j][0]+0.7152*img[i][j][1]+0.0722*img[i][j][2] for j in range(0,len(img[0])-1)]
+    L=sum(Y)/len(Y)
+    return L;
+
+camera=PiCamera()
+
+
+camera.start_preview()
+sleep(5)
+image=camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+
+b_measure(image)
+
+
+
+
+
+
