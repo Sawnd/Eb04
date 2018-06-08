cae15316212fb2c68993c19a76d42e999cefa1bb
Sawnd <sawnd.the.one@gmail.com>
2018-06-08 18:51:35 +0200

    ajout liste des plantes bugs quand actualisation de la paged instertion

--- plante.py
+++ plante.py
@@ -10,6 +10,7 @@ html2 = """
     <title>Ajouter une plante</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">    
     <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
 </head>
 <body>
 <h1> Ajouter une plante </h1>
@@ -25,7 +26,24 @@ html2 = """
     
 """
 
+# Tableaux contenant la liste des plantes
+query2 ='SELECT * FROM plante ORDER BY Nom'
+
+cursor =database.cursor.execute(query2)
+plantes = database.cursor.fetchall()
+liste = """
+<table class='pure-table'><thead><th>Plante</th><th>Temperature</th><th>Humidite</th><th>Ensoleillement</th></thead>
+<tbody>"""
+for p in plantes:
+    liste += "<tr><td>" + str(p['Nom']) + "</td><td>" + str(p['Temperature']) + "</td><td>" +\
+             str(p['Humidite']) + "</td><td>" + str(p["Ensoleillement"]) + "</td></tr>"
+liste += """</tbody>
+</table>
+"""
+
+
 print("Content-type: text/html; charset=utf-8\n")
+html2 += liste
 html2 += """</body>
 </html>"""
 
@@ -35,9 +53,11 @@ nom = str(planteForm.getvalue('nom'))
 temperature = planteForm.getvalue('temperature')
 humidite = planteForm.getvalue('humidite')
 ensoleillement = planteForm.getvalue('ensoleillement')
-database.cursor.execute("INSERT INTO plante (Nom,Temperature,Humidite,Ensoleillement) VALUES ('{0}','{1}','{2}','{3}')"
-                        .format(nom, temperature, humidite, ensoleillement))
-database.db.commit()
+ #On verifie que le formulaire nn'est pas vide
+if(nom != 'None'):
+    database.cursor.execute("INSERT INTO plante (Nom,Temperature,Humidite,Ensoleillement) VALUES ('{0}','{1}','{2}','{3}')"
+                            .format(nom, temperature, humidite, ensoleillement))
+    database.db.commit()
 
 print(html2)
 
