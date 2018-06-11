5c0c323b3d8b3cac220aa3cbefef1c458c1e1453
Sawnd <sawnd.the.one@gmail.com>
2018-06-11 17:50:29 +0200

    ajout connexion à la base de données en remote

--- database.py
+++ database.py
@@ -3,11 +3,16 @@
 #représente la connexion à la base de données
 
 import pymysql.cursors
-# Connect
+# Connexion en local
 db = pymysql.connect(host="localhost",
                        user="root",
                        passwd="",
                        db="eb04",charset="utf8mb4",cursorclass=pymysql.cursors.DictCursor)
+#Connexion sur le raspberry
+#db = pymysql.connect(host="89.84.40.210",
+                       #user="root",
+                       #passwd="pi",
+                       #db="eb04",charset="utf8mb4",cursorclass=pymysql.cursors.DictCursor)
 
 cursor = db.cursor()
 
