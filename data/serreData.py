a64c4bc517deff075ed89195eaa33886ad74eae4
Sawnd <sawnd.the.one@gmail.com>
2018-06-15 01:24:11 +0200

    ajout multi js il faut traiter le formule

---
+++ data/serreData.py
@@ -0,0 +1,32 @@
+import database
+import json
+import http.server
+import html
+
+query1 = 'SELECT * FROM temperature WHERE DATEDIFF(NOW(),Heure) = 0 ORDER BY Heure DESC limit 1'
+query2 = 'SELECT * FROM humidite WHERE DATEDIFF(NOW(),Date) = 0 ORDER BY Date DESC limit 1'
+
+database.cursor.execute(query1)
+results1 = database.cursor.fetchall()
+database.cursor.execute(query2)
+results2 = database.cursor.fetchall()
+
+data = {}
+data["temperatures"] = []
+data["humidites"]= []
+for t in results1:
+    temp = {}
+    temp['valeur'] = t['Valeur']
+    temp['temps'] = t['Heure'].strftime('%Y-%m-%dT%H:%M:%S')
+    data["temperatures"].append(temp)
+for h in results2:
+    hum = {}
+    hum['valeur'] = h['Valeur']
+    hum['temps'] = h['Date'].strftime('%Y-%m-%dT%H:%M:%S')
+    data["humidites"].append(hum)
+
+print("""
+    """)
+#print(html.header)
+print(json.dumps(data, indent=6))
+
--- serre.py
+++ serre.py
@@ -1,6 +1,7 @@
-
+import cgi
 import cgitb
 import plante
+import database
 cgitb.enable()
 
 
@@ -12,9 +13,9 @@ formulaireSerre = """
 <h1> Cr&eacuteer une serre </h1>
 <form class='cf' action='serre.py' method='post'>
             <div class='cf left'>
-           <input type="text" required id ='nom' name='nom' placeholder='Nom'><br>
+           <input type="text" required id ='nom-serre' name='nom-serre' placeholder='Nom'><br>
            <input type="text" disabled placeholder='Plante(s)'><br>
-           <select id="select-serre" multiple ><option=''></option>"""
+           <select id="select-serre" name='select-serre' multiple ><option=''></option>"""
 for p in listePlantes:
     formulaireSerre += "<option value ='" + str(p["ID"]) + "'>" + p["Nom"] +"</option>"
 formulaireSerre += """
@@ -26,3 +27,13 @@ formulaireSerre += """
 """
 html2=formulaireSerre
 print(html2)
+# Traitement du formulaire
+serreForm = cgi.FieldStorage()
+nom = str(serreForm.getvalue('nom-serre'))
+select = serreForm.getvalue('select-serre')
+
+ #On verifie que le formulaire n'est pas vide
+if nom != 'None':
+    database.cursor.execute("INSERT INTO serre (Nom)"
+                            " VALUES ('{0}','{1}','{2}','{3}')".format(nom, temperature, humidite, ensoleillement))
+    database.db.commit()
