600fa5a8794e2a277175ab18e0e6530a147f060c
Sawnd <sawnd.the.one@gmail.com>
2018-06-15 17:36:17 +0200

    finish formulaire serre

--- data/query.py
+++ data/query.py
@@ -3,3 +3,7 @@ query2 ='SELECT * FROM plante ORDER BY Nom'
 
 cursor =database.cursor.execute(query2)
 plantes = database.cursor.fetchall()
+#liste des serres
+query3= 'SELECT * FROM serre '
+cursor=database.cursor.execute(query3)
+liste_serres=database.cursor.fetchall()
--- serre.py
+++ serre.py
@@ -32,7 +32,7 @@ html2 += formulaireSerre
 
 serreForm = cgi.FieldStorage()
 nom = str(serreForm.getvalue('nom-serre'))
-html2+= nom
+html2 += nom
 select = serreForm.getvalue('select-serre')
 
  #On verifie que le formulaire n'est pas vide
@@ -50,5 +50,29 @@ if nom != 'None':
             for plante in select:
                 database.cursor.execute("INSERT INTO serre_plante (IDSerre, IDPlante) VALUES ('{0}','{1}')".format(IDSerre,plante))
                 database.db.commit()
-html2+= html.footer
+html2 += html.footer
+
+#On affiche les serres
+listeSerres=query.liste_serres
+htmlSerre="""<div id='serres'><h1>Mes serres</h1>"""
+for serre in listeSerres:
+    ID_serre = serre['ID']
+    htmlSerre += "<h2>"+serre['Nom']+"""</h2><br><table class='pure-table center ' style="
+    margin:  auto;"
+><thead><th>Plante</th><th>Temperature</th><th>Humidite</th><th>Ensoleillement</th></thead>
+<tbody>"""
+    database.cursor.execute("SELECT serre.Nom AS serre,plante.Nom As plante, plante.Temperature,plante.Humidite,plante.Ensoleillement  FROM serre_plante INNER JOIN plante on serre_plante.IDPlante = plante.ID INNER JOIN serre ON serre_plante.IDSerre = serre.ID WHERE IDSerre='{0}' ".format(ID_serre))
+    listePlantes_serres = database.cursor.fetchall()
+    for plante_serre in listePlantes_serres:
+        htmlSerre += "<tr><td>" +str(plante_serre['plante']) + "</td><td>"+ str(plante_serre['Temperature']) + "</td><td>" + \
+                 str(plante_serre['Humidite']) + "</td><td>" + str(plante_serre["Ensoleillement"]) + "</td></tr>"
+    htmlSerre +="""</tbody></table>"""
+
+html2+=htmlSerre
+
+
+
+
+
+
 print(html2)
